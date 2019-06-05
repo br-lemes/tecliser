d21b626cfaf17f2334ea43478716c3451b0e8006
Breno Ramalho Lemes <breno@br-lemes.net>
2019-06-04 23:44:26 -0400

    Nome da variável igual à versão Lua

--- server/Server.java
+++ server/Server.java
@@ -5,10 +5,10 @@ class Server {
 	public static void main(String[] args) throws Exception {
 		ServerSocket server = new ServerSocket(7070); // Se não der, 6060
 		while (true) {
-			Socket socket = server.accept();
-			System.out.println("\nConnection from: " + socket.getInetAddress().getHostAddress());
-			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			Socket client = server.accept();
+			System.out.println("\nConnection from: " + client.getInetAddress().getHostAddress());
+			DataOutputStream out = new DataOutputStream(client.getOutputStream());
+			BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 			String msg = in.readLine();
 			System.out.println("Received: " + msg);
 			out.writeBytes(msg.toUpperCase() + "\n");
