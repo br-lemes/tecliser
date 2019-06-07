7f8d4cf3538b541baba0c7a458f029769b21cf5d
Breno Ramalho Lemes <breno@br-lemes.net>
2019-06-06 21:02:45 -0400

    Mensages com endereços na versão Lua também

--- 03-normal/server.lua
+++ 03-normal/server.lua
@@ -6,10 +6,11 @@ local server = socket.bind("*", PORT)
 print("Servidor Lua escutando na porta: " .. PORT)
 while true do
 	local client = server:accept()
-	print("\nConexão de: " .. client:getpeername())
+	local address = client:getpeername()
+	print("Conexão de: " .. address)
 	if client then
 		local msg = client:receive()
-		print("Recebido: " .. msg)
+		print(string.format("Recebido de %s: %s", address, msg))
 		if msg then
 			client:send(msg:upper() .. "\n")
 		end
