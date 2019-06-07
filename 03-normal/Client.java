6d5af9d7cba89ed70c0f8425a83c692257e1ab12
Breno Ramalho Lemes <breno@br-lemes.net>
2019-06-06 20:28:21 -0400

    Cliente interativo

--- 03-normal/Client.java
+++ 03-normal/Client.java
@@ -1,11 +1,23 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 class Client {
-	public static void main(String[] args) throws Exception {
-		Socket socket = new Socket("localhost", 7070); // Se não der, 6060
-		new DataOutputStream(socket.getOutputStream()).writeBytes("hello world!\n");
-		System.out.println(
+	static final int PORT = 7070; // Se não der, 6060
+	static final String SERVER = "localhost";
+	public static void main(String[] arg) throws Exception {
+		Socket socket = new Socket(SERVER, PORT);
+		System.out.println("Conectado em " + SERVER + ":" + PORT);
+		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+		if (arg.length > 0) {
+			out.writeBytes(arg[0] + "\n");
+		} else {
+			System.out.print("Digite uma mensagem: ");
+			Scanner in = new Scanner(System.in);
+			String msg = in.nextLine();
+			out.writeBytes(msg + "\n");
+		}
+		System.out.println("Resposta: " +
 			new BufferedReader(new InputStreamReader(
 				socket.getInputStream())
 			).readLine()
