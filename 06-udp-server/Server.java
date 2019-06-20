928726c5834e41518e936d5ce58bd218adcd03d2
Breno Ramalho Lemes <breno@br-lemes.net>
2019-06-20 19:04:12 -0400

    Refatorando...

--- 06-udp-server/Server.java
+++ 06-udp-server/Server.java
@@ -6,21 +6,14 @@ class Server {
 	public static void main(String argv[]) throws Exception {
 		DatagramSocket server = new DatagramSocket(PORT);
 		while(true){
-			// declara o pacote a ser recebido
 			DatagramPacket pkg = new DatagramPacket(new byte[1024], 1024);
-			// recebe o pacote do cliente
 			server.receive(pkg);
-			// pega os dados, o endereço IP e a porta do cliente
-			// para poder mandar a msg de volta
 			String msg = new String(pkg.getData());
-			int port = pkg.getPort();
 			InetAddress address = pkg.getAddress();
 			byte[] data = msg.toUpperCase().getBytes();
-			// monta o pacote com enderço IP e porta
 			DatagramPacket pacoteEnviado = new
 			DatagramPacket(data, data.length,
-			address, port);
-			// envia ao cliente
+			address, pkg.getPort());
 			server.send(pacoteEnviado);
 		}
 	}
