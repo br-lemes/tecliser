cdad38faca32d9a9c4d55057e346bbdc244ddb0f
Breno Ramalho Lemes <breno@br-lemes.net>
2019-06-20 18:53:53 -0400

    Menos espaço em branco, renomeadas variáveis

--- 06-udp-server/Server.java
+++ 06-udp-server/Server.java
@@ -4,38 +4,28 @@ import java.net.*;
 class Server {
 	static final int PORT = 7070; // Se não der, 6060
 	public static void main(String argv[]) throws Exception {
-
-		DatagramSocket servidorSocket = new DatagramSocket(PORT);
-
+		DatagramSocket server = new DatagramSocket(PORT);
 		byte[] dadosRecebidos = new byte[1024];
 		byte[] dadosEnviados = new byte[1024];
 		while(true){
-
 			// declara o pacote a ser recebido
-			DatagramPacket pacoteRecebido = new
-
-			DatagramPacket(dadosRecebidos,dadosRecebidos.length);
+			DatagramPacket pacoteRecebido = new DatagramPacket(dadosRecebidos,dadosRecebidos.length);
 			// recebe o pacote do cliente
-			servidorSocket.receive(pacoteRecebido);
-
+			server.receive(pacoteRecebido);
 			// pega os dados, o endereço IP e a porta do cliente
 			// para poder mandar a msg de volta
 			String sentenca = new String(pacoteRecebido.getData());
-
-			int porta = pacoteRecebido.getPort();
+			int port = pacoteRecebido.getPort();
 			InetAddress enderecoIP = pacoteRecebido.getAddress();
-
 			// transforma em maiúsculas
 			String sentencaCapturada = sentenca.toUpperCase();
 			dadosEnviados = sentencaCapturada.getBytes();
-
 			// monta o pacote com enderço IP e porta
 			DatagramPacket pacoteEnviado = new
 			DatagramPacket(dadosEnviados, dadosEnviados.length,
-			enderecoIP, porta);
-
+			enderecoIP, port);
 			// envia ao cliente
-			servidorSocket.send(pacoteEnviado);
+			server.send(pacoteEnviado);
 		}
 	}
 }
