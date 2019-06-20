52197b3d69af89bf15a747c8e9359031f630e395
Breno Ramalho Lemes <breno@br-lemes.net>
2019-06-20 19:13:45 -0400

    Mensagens informativas

--- 06-udp-server/Server.java
+++ 06-udp-server/Server.java
@@ -5,10 +5,12 @@ class Server {
 	static final int PORT = 7070; // Se não der, 6060
 	public static void main(String argv[]) throws Exception {
 		DatagramSocket server = new DatagramSocket(PORT);
+		System.out.println("Servidor UDP Java escutando na porta: " + PORT);
 		while(true){
 			DatagramPacket pkg = new DatagramPacket(new byte[1024], 1024);
 			server.receive(pkg);
 			String msg = new String(pkg.getData());
+			System.out.println("Recebido de " + pkg.getAddress().getHostAddress() + ": " + msg);
 			byte[] data = msg.toUpperCase().getBytes();
 			server.send(new DatagramPacket(data, data.length, pkg.getAddress(), pkg.getPort()));
 		}
