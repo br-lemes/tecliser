18d3a34bb0184f84b6d1f65ac98e9edb260697ff
Breno Ramalho Lemes <breno@br-lemes.net>
2019-06-20 20:25:24 -0400

    Refatorando...

--- 07-udp-client/Client.java
+++ 07-udp-client/Client.java
@@ -7,12 +7,10 @@ class Client {
 	static final String SERVER = "localhost";
 	public static void main(String[] arg) throws Exception {
 		DatagramSocket client = new DatagramSocket();
-		byte[] recebeDados = new byte[1024];
 		byte[] data = new Scanner(System.in).nextLine().getBytes();
 		DatagramPacket enviaPacote = new DatagramPacket(data, data.length, InetAddress.getByName(SERVER), PORT);
 		client.send(enviaPacote);
-		DatagramPacket recebePacote = new DatagramPacket(recebeDados,
-		recebeDados.length);
+		DatagramPacket recebePacote = new DatagramPacket(new byte[1024], 1024);
 		client.receive(recebePacote);
 		String sentencaModificada = new String(recebePacote.getData());
 		System.out.println("Datagrama UDP enviado com sucesso" +
